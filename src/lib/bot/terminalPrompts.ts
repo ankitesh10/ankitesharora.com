9049be158d4e7729da140d2df2bca923d9e54f43
Ankitesh Arora <ankitesh4@gmail.com>
2026-06-17 17:11:48 +0530

    fix: remove spaces in bot and visitor prompts for consistency

--- src/lib/bot/terminalPrompts.ts
+++ src/lib/bot/terminalPrompts.ts
@@ -1,6 +1,6 @@
 const convertStringToBlue = (text: string) => `\x1B[3;34m${text}\x1B[0m`;
 
-export const botPrompt = convertStringToBlue("aa_bot@ ankitesharora.com:~$ ");
+export const botPrompt = convertStringToBlue("aa_bot@ankitesharora.com:~$ ");
 export const visitorPrompt = convertStringToBlue(
-  "visitor@ ankitesharora.com:~$ ",
+  "visitor@ankitesharora.com:~$ ",
 );
